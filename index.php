8c0eeb3a62e0da960915eb643f7676d6e3b866d0
Rajesh  D <rajeshkannan7157@gmail.com>
2023-07-11 07:34:07 +0000

    I added a Table File

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<html>
 <?php
+$table=$_GET['table'];
+$rows=100;
+?>
 
-echo"Rajesh";
+<head>
+	<title>sample</title>
+</head>
+
+<body>
+	<table>
+		<tr>
+			<th>Multiplicant</th>
+			<th></th>
+			<th>Multiplier</th>
+			<th>Value</th>
+		</tr>
+		<?php
+for ($i=0;$i<=$rows;$i++) {
+    ?>
+		<tr>
+			<td>
+				<?=$i?>
+			</td>
+			<td>x</td>
+			<td>
+				<?=$table?>
+			</td>
+			<td>
+				<?=$table*$i?>
+			</td>
+		</tr>
+		<?php
+}
+?>
+	</table>
+</body>
+
+</html>
